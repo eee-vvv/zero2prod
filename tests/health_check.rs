2eee4643d8bbca98f02e45ff5aeeed1500ae0713
eeevvv <evan.w.and@gmail.com>
2022-03-16 16:33:11 -0400

    update github actions workflow

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -35,7 +35,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
