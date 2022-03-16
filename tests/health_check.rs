3e0f7c919288ce6443485e42ed14bbd0245b8345
eeevvv <evan.w.and@gmail.com>
2022-03-15 21:51:34 -0400

    first integration test for health_check running

---
+++ tests/health_check.rs
@@ -0,0 +1,28 @@
+//! tests/health_check.rs
+
+#[actix_web::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    // bringing in `reqwest` to perform HTTP requests
+    // against application
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch application in the background
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    //Launch the server as a background task
+    let _ = tokio::spawn(server);
+}
