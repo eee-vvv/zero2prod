1f2dcccfd0621e3ed7197941860bca2386cc77cf
eeevvv <evan.w.and@gmail.com>
2022-03-15 22:14:22 -0400

    formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,8 +21,7 @@ async fn health_check_works() {
 
 // Launch application in the background
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
